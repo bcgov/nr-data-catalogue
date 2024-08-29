23b743268fdbf06b2c4ad20b3167e3956119c66e
Athwal <Jastinder.Athwal@gov.bc.ca>
2024-08-29 13:23:55 -0700

    user add/remove functions added

--- scripts/config.py
+++ scripts/config.py
@@ -9,8 +9,11 @@ base_url = "https://nr-data-catalogue-dev.apps.emerald.devops.gov.bc.ca/api/v1"
 # Database schema to filter by
 database_schema = "your_schema_here"
 
+# Selected user ID (replace with actual user ID)
+selected_user_id = "your_user_id_here"
+
 # API key for authentication
-api_key = ""
+api_key = "your_token_here"
 
 # API request headers for get requests 
 headers_get = {
