74f5a39b400f972334aaa4c7b7b1ff46511eed3b
Athwal <Jastinder.Athwal@gov.bc.ca>
2024-11-29 09:16:32 -0800

    applying users to applications started

--- scripts/config.py
+++ scripts/config.py
@@ -7,7 +7,7 @@ import time
 import logging
 from datetime import datetime
 
-env = 'dev'
+env = 'test'
 
 # API base URL TEST ENV
 base_url = f"https://nr-data-catalogue-{env}.apps.emerald.devops.gov.bc.ca/api/v1"
@@ -19,7 +19,7 @@ database_schema = f"ODS.ods{env}.ats_replication"
 selected_user_id = "your_user_id_here"
 
 # API key for authentication
-api_key = "your_api_key_here"
+api_key = "eyJraWQiOiJHYjM4OWEtOWY3Ni1nZGpzLWE5MmotMDI0MmJrOTQzNTYiLCJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJvcGVuLW1ldGFkYXRhLm9yZyIsInN1YiI6Imphc3RpbmRlci5hdGh3YWwiLCJyb2xlcyI6WyJOUk0gRGF0YSBDb25zdW1lciBSb2xlIiwiQWRtaW4iXSwiZW1haWwiOiJqYXN0aW5kZXIuYXRod2FsQGdvdi5iYy5jYSIsImlzQm90IjpmYWxzZSwidG9rZW5UeXBlIjoiUEVSU09OQUxfQUNDRVNTIiwiaWF0IjoxNzMyNzMyMDAzLCJleHAiOjE3MzUzMjQwMDN9.vz3So31Pi6IgsxLLIa-1kpQICnlccVExh9lpeDHx4TCWY2qxQ5ZzJWlc2yBh6YiwY7cozdurJ4jhwD05f9x3Xgb2cSwCwNDh_Kb0I4dvPsfONOs_a_cUm3sQV3COoQxEFYKXr78eW2lwYsDOAtAdRfINWHp_Np9lX30oLHsJgXD4E6hG6EGIb5PJJQ2TK6UuVAa5rWWD8gJ_z4CSEfAZNmYF5sSe2OJpxfsjKYrRkDYjf05jhdHM7Da-90wSPSfarXPYFQhYaXKyW7_Js9MBXEG5qvWt6rd7ps9jXz1xl3Lm1_r92sGtl8cFNmHpDJI1rMnvvp8RXXUoR9uCa5DJ2g"
 
 # API request headers for get requests 
 headers_get = {
